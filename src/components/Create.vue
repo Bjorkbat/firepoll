b8df3b602ad76e3457858ae0da4124dbb6332809
Bjorkbat <bjorkbat@gmail.com>
2017-09-21 16:27:03 -0600

    Added the create page

---
+++ src/components/Create.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Create A Poll</h1>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+import Create from 'components/Create.vue';
+
 Vue.use(Router);
 
 export default new Router({
-  routes: []
+  routes: [
+    {
+      path: '/',
+      component: Create
+    }
+  ]
 });
