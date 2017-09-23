07ba0fa0a959299c3456186966d496c98a344a5a
Bjorkbat <bjorkbat@gmail.com>
2017-09-23 10:57:50 -0600

    Added route for poll

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Create from 'components/Create.vue';
+import Poll from 'components/Poll.vue';
 
 Vue.use(Router);
 
@@ -10,6 +11,10 @@ export default new Router({
     {
       path: '/',
       component: Create
+    },
+    {
+      path: '/p/:id',
+      component: Poll
     }
   ]
 });
