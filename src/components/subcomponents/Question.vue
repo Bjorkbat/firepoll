0e4b9682a051f7a98af374d51cf214cdad91bdcc
Bjorkbat <bjorkbat@gmail.com>
2017-09-23 10:57:30 -0600

    Much happier with the visual appearanace of the question component

--- src/components/subcomponents/Question.vue
+++ src/components/subcomponents/Question.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <h5>{{question.question}}</h5>
-    <ul class="list-unstyled" text-align="left">
-      <li v-for="response in question.responses">{{response}}</li>
+    <ul class="list-unstyled">
+      <li v-for="response in question.responses" class="h6">{{response}}</li>
     </ul>
   </div>
 </template>
@@ -14,7 +14,26 @@ export default {
 </script>
 
 <style scoped>
+
 div {
-  border: 1px solid #CCC;
+  background-color: #FFDC45;
+  border: 1px solid #FFD000;
+  border-radius: 10px;
+  box-shadow: 1px 1px 1px #CCC;
+  margin: 10px auto;
+  padding: 10px;
 }
+
+ul {
+  text-align: left;
+}
+
+li {
+  background-color: #FFE577;
+  border: 1px solid #FFFAE7;
+  border-radius: 4px;
+  box-shadow: 1px 1px 1px #CCC;
+  padding: 10px;
+}
+
 </style>
